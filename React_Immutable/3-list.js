fc04bbdbbdadb8b6762120ec03a79a9dd5ae9337
barasa001 <s.wanyonyi@alustudent.com>
2024-05-26 09:37:33 +0300

    made updates

--- React_Immutable/3-list.js
+++ React_Immutable/3-list.js
@@ -1,9 +1,9 @@
 import { List } from 'immutable';
 
 export function getListObject(array) {
-  return List(array);
+	return List(array);
 }
 
 export function addElementToList(list, element) {
-  list.push(element);
+	return list.push(element);
 }
--- React_Immutable/8-seq.js
+++ React_Immutable/8-seq.js
@@ -1,20 +1,15 @@
-import { Map, Seq } from 'immutable';
+import { Seq } from 'immutable';
 
-function printBestStudents(object) {
-    const bestStudents = Seq(Map(object))
-        .filter((student) => student.score >= 70)
-        .map((student) => ({
-            score: student.score,
-            firstName: `${student.firstName[0].toUpperCase()}${student.firstName.slice(1)}`,
-            lastName: `${student.lastName[0].toUpperCase()}${student.lastName.slice(1)}`,
-        }));
-    console.log(bestStudents.toObject());
+export default function printBestStudents(grades) {
+	const bestStudents = Seq(grades)
+		.filter((student) => student.score >= 70)
+		.map((student) => ({
+			score: x.score,
+			firstName:
+				student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
+			lastName:
+				student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
+		}));
+
+	console.log(bestStudents.toObject());
 }
-
-// printBestStudents({
-//     1: {
-//       score: 99,
-//       firstName: 'guillaume',
-//       lastName: 'salva',
-//     }
-// })
